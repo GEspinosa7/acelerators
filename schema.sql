3843a3b5989adde11b3001c349cb223cf6347c1e
Gabriel Espinosa <gabrielespinosajob@gmail.com>
2021-12-06 17:44:46 -0300

    update

--- schema.sql
+++ schema.sql
@@ -2,12 +2,12 @@ create database acelerator;
 
 create table if not exists types(
   id serial not null primary key,
-  title varchar(150) not null,
+  title varchar(150) not null
 );
 
 create table if not exists modules(
   id serial not null primary key,
-  title varchar(150) not null,
+  title varchar(150) not null
 );
 
 create table if not exists acelerators(
@@ -18,5 +18,7 @@ create table if not exists acelerators(
   types_id integer not null,
   modules_id integer not null,
   foreign key(types_id) references types(id),
-  foreign key(module) references modules(id)
+  foreign key(modules_id) references modules(id)
 );
+
+
